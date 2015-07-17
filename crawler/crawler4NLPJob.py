bf714118e50233ca384ae11d8a272bd0c0d827ac
cheesezhe <zh199104@gmail.com>
2015-07-17 08:23:43 +0800

    update

--- crawler/crawler4NLPJob.py
+++ crawler/crawler4NLPJob.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+#coding: utf-8
 __author__ = 'zhanghe'
+#7426-17189
+#http://www.nlpjob.com/job/7426/
+import urllib2
+url = "http://www.nlpjob.com/job/7426/"
+request = urllib2.Request(url)
+response = urllib2.urlopen(request)
+html = response.read()
+print html
